839580f1a446d127de74e8e1916a7dc0316c6a7a
Guilherme <guilherme@Guilhermes-MacBook-Pro.local>
2020-09-25 18:26:34 -0300

    First Commit

---
+++ Variáveis/script.js
@@ -0,0 +1,10 @@
+var nome = "Guilherme";
+var idade = "24";
+var comidaFavorita;
+comidaFavorita = "Lanche";
+
+var escolaSelecionada;
+var cursoSelecionado;
+var horarioSelecionado;
+var veiculoSelecionado;
+var corSelecionada;
